c14c155ed5bc6498782380c5363c1b2c84f3426d
prb01 <p.bergstroem@gmail.com>
2021-12-17 09:19:18 +0100

    Playwright class added with :: and # methods
    plays.db > playwrights
     ::all -> return all data from table
     ::find_by_name -> return all data by playwright name
     #new -> initialize method
     #create -> insert new row in table
     #update -> update row in table
     #get_plays -> return all plays written by playwright

--- plays.rb
+++ plays.rb
@@ -75,3 +75,59 @@ class Play
     SQL
   end
 end
+
+class Playwright
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+    SELECT
+      *
+    FROM
+      playwrights
+    WHERE
+      name = ?
+    SQL
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  attr_reader :id
+  attr_accessor :name, :birth_year
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+    INSERT INTO
+      playwrights (name, birth_year)
+    VALUES
+      (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+    UPDATE
+      playwrights
+    SET
+      name = ?,
+      birth_year = ?
+    WHERE
+      id = ?
+      SQL
+  end
+
+  def get_plays
+    data = Play.find_by_playwright(self.name)
+  end
+end
