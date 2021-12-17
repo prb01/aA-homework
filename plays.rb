ccacb93915f037cf6a3d39f4778865e314733530
prb01 <p.bergstroem@gmail.com>
2021-12-17 08:44:06 +0100

    Initial commit for plays exercise

--- plays.rb
+++ plays.rb
@@ -19,6 +19,18 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    data = PlayDBConnection.instance.execute(<<-SQL, title)
+    SELECT
+      *
+    FROM
+      plays
+    WHERE
+      title = ?
+    SQL
+    data.map { |datum| Play.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
