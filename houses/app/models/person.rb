2154adb14a61691f3c01662c507e40c1f8de88aa
prb01 <p.bergstroem@gmail.com>
2021-12-29 09:36:08 +0100

    Person & House associates added

--- houses/app/models/person.rb
+++ houses/app/models/person.rb
@@ -1,4 +1,10 @@
 class Person < ApplicationRecord
   validates :name, presence: true
   validates :house_id, presence: true
+
+  belongs_to(
+    :house,
+    class_name: 'House',
+    foreign_key: :house_id,
+    primary_key: :id)
 end
